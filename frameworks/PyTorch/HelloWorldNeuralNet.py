d4427aea30f0e0ca8cfdea6da29a84886cd56982
ccampell <ccampell.asu@gmail.com>
2018-03-21 03:47:24 -0400

    Added Hello World code for PyTorch.

---
+++ frameworks/PyTorch/HelloWorldNeuralNet.py
@@ -0,0 +1,8 @@
+"""
+HelloWorldNeuralNet.py
+Follows the example online at: http://pytorch.org/tutorials/beginner/blitz/neural_networks_tutorial.html
+"""
+
+import torch
+from torch import autograd
+
