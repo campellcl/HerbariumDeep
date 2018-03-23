3cba521fb0afaf75a6ac1bded55086622acbc36f
ccampell <ccampell.asu@gmail.com>
2018-03-23 18:45:19 -0400

    Added the network.

--- frameworks/PyTorch/HelloWorldNeuralNetworkClassifier.py
+++ frameworks/PyTorch/HelloWorldNeuralNetworkClassifier.py
@@ -19,6 +19,62 @@ import torchvision.transforms as transforms
 import matplotlib.pyplot as plt
 import numpy as np
 
+from torch.autograd import Variable
+import torch.nn as nn
+import torch.nn.functional as F
+
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        # Input -> C1: feature maps 6 @ (28 x 28)
+        # 3 input image channels, 6 output layers, 5x5 square convolution kernel
+        self.conv1 = nn.Conv2d(in_channels=3, out_channels=6, kernel_size=5)
+
+        # Max pooling over a (2, 2) window
+        self.pool = nn.MaxPool2d(kernel_size=(2, 2))
+
+        # C1 -> C3: feature maps 16 @ (10 x 10)
+        # 6 input channels, 16 output layers, 5x5 square convolution kernel
+        self.conv2 = nn.Conv2d(in_channels=6, out_channels=16, kernel_size=5)
+
+        # C3 -> S4: feature maps 16 @ (5 x 5) <last feature map layer>
+        self.fc1 = nn.Linear(in_features=16 * 5 * 5, out_features=120)
+
+        # S4 -> C5: Fully Connected Layer (120 x 1)
+        self.fc2 = nn.Linear(in_features=120, out_features=84)
+
+        # C5 -> F6: Fully Connected Layer (84 x 1)
+        self.fc3 = nn.Linear(in_features=84, out_features=10)
+
+    def forward(self, x):
+        """
+        forward: Performs a forward pass through the network. Architecturally, this method defines the connections
+            between layers in the network (how to get the output of the neural net). This function is called when
+            applying the neural net to an input autograd.Variable:
+                net = Net()
+                net(input) # calls net.forward(input)
+        :param x: The input to the neural network.
+        :return x: The input to the neural network after undergoing a forward pass through the net.
+        """
+        '''
+        F is a module housing functions (such as pooling) as well as various activation functions. 
+        '''
+        # Max pooling over a (2, 2) window during the forward pass:
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        '''
+        The view function takes a Tensor and re-shapes it. Here we are resizing x to a matrix of size:
+            (-1 x self.num_flat_features(x)). Of course the -1 isn't really negative one; it means to auto-infer the 
+            dimensionality from the other dimensions (see: http://pytorch.org/docs/master/tensors.html#torch.Tensor.view)
+        In this particular case the inferred dimension is 1, as we are changing x to a vector with 
+            (1 x self.num_flat_features(x))
+        '''
+        x = x.view(-1, (16 * 5 * 5))
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        # Here we pass through the output layer and return the result:
+        x = self.fc3(x)
+        return x
 
 '''
 We can view some of the training images:
@@ -28,7 +84,6 @@ def imshow(img):
     npimg = img.numpy()
     plt.imshow(np.transpose(npimg, (1, 2, 0)))
     plt.show()
-
 
 def main():
     trainloader = torch.utils.data.DataLoader(trainset, batch_size=4, shuffle=True, num_workers=2)
@@ -45,8 +100,6 @@ def main():
 
     # show images
     imshow(torchvision.utils.make_grid(images))
-
-
 
 '''
 The torchvision package contains the CIFAR10 dataset. This dataset has the classes: 
@@ -74,6 +127,8 @@ if __name__ == '__main__':
 
     main()
 
+# instantiate the network:
+net = Net()
 
 
 
