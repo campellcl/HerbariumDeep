0b1487b46d3ed9c0b83d7ee5c3e904fff640c248
ccampell <ccampell.asu@gmail.com>
2018-04-05 23:50:40 -0400

    Almost finished the first layer. Research is needed to understand LRN, and RELU. Not sure where to put LRN for pytorch. Perhaps the first layer should be sequential and not simply method declarations.

--- frameworks/PyTorch/GoogLeNet.py
+++ frameworks/PyTorch/GoogLeNet.py
@@ -38,18 +38,17 @@ class GoogLeNet(nn.Module):
 
     def __init__(self):
         super(GoogLeNet, self).__init__()
-        # The size of the receptive field is 224 x 224 with 3 channel RGB and normalization via mean subtraction.
-        # TODO: Where is the receptive field loaded?
-
-        # the pre-layers are the initial pre-processing steps.
-        self.pre_layers = nn.Sequential(
-            nn.Conv2d(in_channels=3, )
-            # Performing a Batch2d normalization standardizes pixel intensities. We don't want to learn to tell day
-            #   from night, we want the features in the image. Not the discrepancy between intensities.
-        )
-        self.conv1 = nn.Conv2d(in_channels=3, out_channels=())
+        # conv2d 0
+        # TODO: Two implementations say padding should be 3 here, one says p=1.
+        # TODO: Why would this be the case? Do the Andrew Ng math.
+        self.conv0 = nn.Conv2d(in_channels=3, out_channels=64, kernel_size=(7, 7), stride=2, padding=3)
+        # Note: the ReLU would be defined here but it is run during the call to the forward pass function.
+        self.max_pool0 = nn.MaxPool2d(kernel_size=(3, 3), stride=2, padding=1)
+        # TODO: What is the Local Resp Norm? Can still issue library calls but would be good to know the math.
+        # self.loc_resp_norm = nn.CrossMapLRN2d(size=)
 
     def forward(self, x):
+        output = self.max_pool0(F.relu(self.conv0(x)))
         return x
 
 if __name__ == '__main__':
@@ -63,7 +62,9 @@ if __name__ == '__main__':
     # channel_means = [trainset.train_data[...,i].mean() for i in range(trainset.train_data.shape[-1])]
 
     transform = transforms.Compose(
-        [transforms.ToTensor(),
+        [transforms.Scale(load_size),
+         transforms.CenterCrop(crop_size),
+         transforms.ToTensor(),
          transforms.Normalize(mean=[125.306918046875, 122.950394140625, 113.86538318359375], std=[1, 1, 1])])
 
     # Download (or load if already present) the CIFAR10 training dataset:
@@ -72,4 +73,6 @@ if __name__ == '__main__':
     # Download (or load if already present) the CIFAR10 test dataset:
     testset = torchvision.datasets.CIFAR10(root='./demos/data', train=False, download=True, transform=transform)
 
+    # Load the dataset while applying the specified transform:
+    # train_loader = torch.utils.data.DataLoader(root='./demos/data')
 
