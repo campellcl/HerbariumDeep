408d673f2b993db20f383bd5b19b35fd21b1b362
ccampell <ccampell.asu@gmail.com>
2018-07-01 15:02:01 -0400

    Re-writing web scraper to avoid relying on manually generated csv. Web scraper is to be fully automated and will update accordingly without human intervention.

--- frameworks/DataAcquisition/Scrapy/WebScraper.py
+++ frameworks/DataAcquisition/Scrapy/WebScraper.py
@@ -7,9 +7,15 @@ import os
 import argparse
 from collections import OrderedDict
 from scrapy import Selector
+from scrapy.http import HtmlResponse
 import urllib3
 import certifi
+import requests
+import xml.etree.ElementTree as ET
+import lxml
 import pandas as pd
+import numpy as np
+import json
 
 '''
 Command line argument parsers:
@@ -54,18 +60,80 @@ def init_storage_dir(inst, code):
 
 
 def main():
-    # Open a csv of SERNEC institutions and institution codes:
-    with open('../../../data/SERNEC/InstitutionCodes/InstitutionCodes.csv', 'r') as fp:
-        institution_codes = pd.read_csv(fp, header=0, dtype={'institution': str, 'code': str})
-        # Remove any trailing or starting white spaces in the institution codes:
-        institution_codes['code'] = institution_codes['code'].str.strip()
-    # Go through every institution:
-    for index, row in institution_codes.iterrows():
-        inst = row[0]
-        code = row[1]
-        # Setup directory and metadata for institution if not already present:
-        init_storage_dir(inst, code)
-    # ?
+    """
+    main: Scrapes and assembles a meta dataframe containing the collection identifiers for every SERNEC collection and
+        other additional information for each collection such as:
+        * Collection Identifier (collid): A three digit numeric unique identifier for the collection.
+        * Institution Code (inst): The institution code associated with the collection (i.e. BOON).
+        * Description (desc): The full name of the collection (i.e. Appalachian State University Herbarium).
+        * Emllink (emllink): A link to the sernec portal containing collection stats and more metadata.
+        * Darwin Core Archive (dwca): A link to the metadata for this collection in the form of a DwC-A export.
+    :return:
+    """
+    # Scrapes the collection identifiers for every SERNEC collection.
+    df_collids = pd.DataFrame(columns={'collid', 'inst', 'desc', 'emllink', 'dwca'})
+    # Instantiate http object per urllib3:
+    http = urllib3.PoolManager(
+        cert_reqs = 'CERT_REQUIRED',
+        ca_certs = certifi.where()
+    )
+    # Obtain list of SERNEC collection identifiers (collids):
+    if args.verbose:
+        print('Issuing GET request to global source URL: %s' % args.source_url)
+    collids_response = http.request('GET', 'http://sernecportal.org/portal/collections/datasets/rsshandler.php')
+    if args.verbose:
+        print('Received HTTP response code: %d' % collids_response.status)
+        # print(collids_response.data)
+    rss_root = ET.fromstring(collids_response.data)
+    root = rss_root.getchildren()[0]
+    for child in root:
+        if child.tag == 'item':
+            collid = int(child.attrib['collid'])
+            if args.verbose:
+                print('Parsing XML response for child %s' % collid)
+            for property in child.getchildren():
+                if property.tag == 'title':
+                    title = property.text
+                elif property.tag == 'description':
+                    description = property.text
+                elif property.tag == 'emllink':
+                    emllink = property.text
+                elif property.tag == 'link':
+                    link = property.text
+            # Get the DWCA link for this collection:
+            if '.zip' in link:
+                if args.verbose:
+                    print('\tLocated DwC-A link for COLLID: %s, INST: %s, at: %s' % (collid, title, link))
+                coll_series = pd.Series(
+                    {'collid': collid, 'inst': title, 'desc': description, 'emllink': emllink, 'dwca': link}
+                )
+            else:
+                # NOTE: Tried web scraping for DwC-A, but if it isn't present under 'link' than the data is a EML File.
+                if args.verbose:
+                    print('\tThis collection: %s, INST: %s has no publicly available DwC-A. '
+                          'This collection will be omitted from global data frame.')
+                # if args.verbose:
+                #     print('\tIssuing GET request to DwC-A source URL: %s' % link)
+                #     print('\tAttempting to parse HTML for DwC-A source URL')
+                # dwca_response = requests.get(link)
+                # Selector(response=dwca_response).xpath('//body').extract()
+                # Convert html byte repsonse to string type and then to a dict for json encoding:
+                # json_res = json.loads(dwca_response.text)
+
+            df_collids = df_collids.append(coll_series, ignore_index=True)
+    pass
+    # # Open a csv of SERNEC institutions and institution codes:
+    # with open('../../../data/SERNEC/InstitutionCodes/InstitutionCodes.csv', 'r') as fp:
+    #     institution_codes = pd.read_csv(fp, header=0, dtype={'institution': str, 'code': str})
+    #     # Remove any trailing or starting white spaces in the institution codes:
+    #     institution_codes['code'] = institution_codes['code'].str.strip()
+    # # Go through every institution:
+    # for index, row in institution_codes.iterrows():
+    #     inst = row[0]
+    #     code = row[1]
+    #     # Setup directory and metadata for institution if not already present:
+    #     init_storage_dir(inst, code)
+    # # ?
 
 
     # http = urllib3.PoolManager(
