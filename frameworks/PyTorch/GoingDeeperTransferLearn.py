80d8d705689a9454510c3873241026193f9bf6d8
ccampell <ccampell.asu@gmail.com>
2018-07-30 20:05:01 -0400

    Added code to partition the files on the hard drive into train and test sets. First df_meta is partitioned into df_train and df_test. Both new data frames are saved and the file_path column is updated to the new location of the files after partitioning.

--- frameworks/PyTorch/GoingDeeperTransferLearn.py
+++ frameworks/PyTorch/GoingDeeperTransferLearn.py
@@ -12,6 +12,8 @@ import numpy as np
 from scipy.stats import mode
 from itertools import permutations, combinations
 from math import ceil
+from sklearn import model_selection
+import shutil
 
 model_names = sorted(name for name in models.__dict__
                      if name.islower() and not name.startswith("__")
@@ -86,86 +88,85 @@ def sort_by_target_then_collector(df_meta):
     return df_meta_sorted
 
 
-def partition_data(df_meta):
-    """
-    partition_data: Partitions the metadata into training, testing, and validation datasets. Partitioning is performed
-        secondarily by target label (dwc:scientificName) in an attempt to keep classes equally represented in both
-        training and testing sets. Partitioned is performed primarily by collector so that no single collector has
-        entries in both the training and testing datasets. This is done to avoid bias in the classifier. It is not
-        desired that the classifier learns to predict samples only collected by a certain type of collector (i.e. those
-        that follow a particular style of sample mounting). By isolating collectors' samples to either training or
-        test sets in their entirely the algorithm has less of a chance to pick up on patterns among collectors and can
-        instead focus on learning patterns among target labels (species). This method will update the respective,
-        metadata dataframe's 'file_path' attribute to house the new location of the samples after partitioning.
-    :param df_meta: The global metadata dataframe.
-    :returns Up to 3 instances of the metadata partitioned into separate dataframes:
-        :return df_meta_train:
-        :return df_meta_test:
-        :return df_meta_val:
-    """
-    species_coll_counts = {}
-    # Sort the dataframe first by the class label and then by collector:
-    df_meta = sort_by_target_then_collector(df_meta)
-    total_num_target_labels = len(df_meta['dwc:scientificName'].unique())
-    for i, target_label in enumerate(df_meta['dwc:scientificName'].unique()):
-        if target_label is not np.nan:
-            coll_counts = {}
-            target_subset = df_meta[df_meta['dwc:scientificName'] == target_label]
-            # Drop entries with a collector of np.nan from the target label subset:
-            target_subset = target_subset.dropna(axis=0, how='any', subset=['dwc:recordedBy'])
-            for collector in target_subset['dwc:recordedBy'].unique():
-                collector_samples = target_subset[target_subset['dwc:recordedBy'] == collector]
-                coll_counts[collector] = collector_samples.shape[0]
-            species_coll_counts[target_label] = coll_counts
-            # print('collector counts for target label [%s]: %s' % (target_label, coll_counts))
-            print('Finished partitioning samples by collector for target label <%d/%d>: [%s]'
-                  % (i+1, total_num_target_labels, target_label))
-            print('\tThere are %d samples for this target label with a non-NaN collector.'
-                  % target_subset.shape[0])
-            print('\tThere are %d collectors for this target label.' % len(coll_counts))
-            collection_counts = list(coll_counts.values())
-            if collection_counts:
-                print('\tThe min number of samples recorded by an individual collector was: %d'
-                      % min(collection_counts))
-                print('\tThe max number of samples recorded by an individual collector was: %d.'
-                      % max(collection_counts))
-                print('\tThe mean number of samples recorded by an individual collector was: %d'
-                      % np.mean(collection_counts))
-                print('\tThe median number of samples recorded by an individual collector was: %d'
-                      % np.median(collection_counts))
-                # print('\tThe mode number of samples recorded by an individual collector was: %d'
-                #       % mode(list(coll_counts.values())))
-                ''' partition collectors into training or testing based on number of samples '''
-                # Sort the target_subset by collector and number of observations:
-                coll_counts = target_subset['dwc:recordedBy'].value_counts(dropna=True)
-                coll_counts = coll_counts[coll_counts.values != 0]
-                total_num_samples = target_subset.shape[0]
-                percent_test = 20
-                ideal_num_test_samples = ceil((percent_test * total_num_samples)/100)
-                ideal_num_train_samples = total_num_samples - ideal_num_test_samples
-                RESUME HERE
-                NEED TO FIGURE OUT HOW TO PERMUTE THE DATA PROPERLY
-                OR IMPLEMENT THE GREEDY APPROACH AND OBTAIN 20 PERCENT AND STORE REST IN TRAIN
-                # Get every possible permutation of the collectors and their samples (order matters):
-                train_test_perms = permutations(coll_counts.items(), r=coll_counts.shape[0])
-                # train_test_perms = [dict(zip(coll_counts, v)) for v in permutations(coll_counts.values(), r=2)]
-                for p, perm in enumerate(train_test_perms):
-                    print('permutation: %s' % (perm, ))
-                #     ratio = coll_counts[perm[0]]/coll_counts[perm[1]]
-                #     pass
-                pass
-                # sorted_coll_counts = [(coll, coll_counts[coll]) for coll in sorted(coll_counts, key=coll_counts.get, reverse=)]
-    pass
-
-
-            # TODO: how to divide the collector subsets to maintain 80/20 ratio?
-    # Collect statistics on the number of viable class labels:
-    # TODO: Partition so that collector is isolated and target labels equally represented in both train and test splits.
-    # TODO: Actually move the data on the hard drive to the appropriate folders.
-    # TODO: Update the columns of the respective dataframe to the new sample locations
-    # TODO: Update the global metadata dataframe if necessary.
-    # TODO: Return df_meta_train, df_meta_test, df_meta_val.
-    return NotImplementedError
+# def partition_data(df_meta):
+#     """
+#     partition_data: Partitions the metadata into training, testing, and validation datasets. Partitioning is performed
+#         secondarily by target label (dwc:scientificName) in an attempt to keep classes equally represented in both
+#         training and testing sets. Partitioned is performed primarily by collector so that no single collector has
+#         entries in both the training and testing datasets. This is done to avoid bias in the classifier. It is not
+#         desired that the classifier learns to predict samples only collected by a certain type of collector (i.e. those
+#         that follow a particular style of sample mounting). By isolating collectors' samples to either training or
+#         test sets in their entirely the algorithm has less of a chance to pick up on patterns among collectors and can
+#         instead focus on learning patterns among target labels (species). This method will update the respective,
+#         metadata dataframe's 'file_path' attribute to house the new location of the samples after partitioning.
+#     :param df_meta: The global metadata dataframe.
+#     :returns Up to 3 instances of the metadata partitioned into separate dataframes:
+#         :return df_meta_train:
+#         :return df_meta_test:
+#         :return df_meta_val:
+#     """
+#     species_coll_counts = {}
+#     # Sort the dataframe first by the class label and then by collector:
+#     df_meta = sort_by_target_then_collector(df_meta)
+#     total_num_target_labels = len(df_meta['dwc:scientificName'].unique())
+#     for i, target_label in enumerate(df_meta['dwc:scientificName'].unique()):
+#         if target_label is not np.nan:
+#
+#             coll_counts = {}
+#             target_subset = df_meta[df_meta['dwc:scientificName'] == target_label]
+#             # Drop entries with a collector of np.nan from the target label subset:
+#             target_subset = target_subset.dropna(axis=0, how='any', subset=['dwc:recordedBy'])
+#             for collector in target_subset['dwc:recordedBy'].unique():
+#                 collector_samples = target_subset[target_subset['dwc:recordedBy'] == collector]
+#                 coll_counts[collector] = collector_samples.shape[0]
+#             species_coll_counts[target_label] = coll_counts
+#             # print('collector counts for target label [%s]: %s' % (target_label, coll_counts))
+#             print('Finished partitioning samples by collector for target label <%d/%d>: [%s]'
+#                   % (i+1, total_num_target_labels, target_label))
+#             print('\tThere are %d samples for this target label with a non-NaN collector.'
+#                   % target_subset.shape[0])
+#             print('\tThere are %d collectors for this target label.' % len(coll_counts))
+#             collection_counts = list(coll_counts.values())
+#             if collection_counts:
+#                 print('\tThe min number of samples recorded by an individual collector was: %d'
+#                       % min(collection_counts))
+#                 print('\tThe max number of samples recorded by an individual collector was: %d.'
+#                       % max(collection_counts))
+#                 print('\tThe mean number of samples recorded by an individual collector was: %d'
+#                       % np.mean(collection_counts))
+#                 print('\tThe median number of samples recorded by an individual collector was: %d'
+#                       % np.median(collection_counts))
+#                 # print('\tThe mode number of samples recorded by an individual collector was: %d'
+#                 #       % mode(list(coll_counts.values())))
+#                 ''' partition collectors into training or testing based on number of samples '''
+#                 # Sort the target_subset by collector and number of observations:
+#                 coll_counts = target_subset['dwc:recordedBy'].value_counts(dropna=True)
+#                 coll_counts = coll_counts[coll_counts.values != 0]
+#                 total_num_samples = target_subset.shape[0]
+#                 percent_test = 20
+#                 ideal_num_test_samples = ceil((percent_test * total_num_samples)/100)
+#                 ideal_num_train_samples = total_num_samples - ideal_num_test_samples
+#
+#                 # Get every possible permutation of the collectors and their samples (order matters):
+#                 train_test_perms = permutations(coll_counts.items(), r=coll_counts.shape[0])
+#                 # train_test_perms = [dict(zip(coll_counts, v)) for v in permutations(coll_counts.values(), r=2)]
+#                 for p, perm in enumerate(train_test_perms):
+#                     print('permutation: %s' % (perm, ))
+#                 #     ratio = coll_counts[perm[0]]/coll_counts[perm[1]]
+#                 #     pass
+#                 pass
+#                 # sorted_coll_counts = [(coll, coll_counts[coll]) for coll in sorted(coll_counts, key=coll_counts.get, reverse=)]
+#     pass
+#
+#
+#             # TODO: how to divide the collector subsets to maintain 80/20 ratio?
+#     # Collect statistics on the number of viable class labels:
+#     # TODO: Partition so that collector is isolated and target labels equally represented in both train and test splits.
+#     # TODO: Actually move the data on the hard drive to the appropriate folders.
+#     # TODO: Update the columns of the respective dataframe to the new sample locations
+#     # TODO: Update the global metadata dataframe if necessary.
+#     # TODO: Return df_meta_train, df_meta_test, df_meta_val.
+#     return NotImplementedError
 
 
 def update_metadata_with_file_paths(df_meta):
@@ -208,6 +209,72 @@ def update_metadata_with_file_paths(df_meta):
     return df_meta
 
 
+def partition_data():
+    # Data needs to be split into train, test, validate groups.
+    # Data should be split so that no collector ends up in both the training and testing sets.
+    # Load the metadata dataframe holding the collector of each sample:
+    if os.path.isfile(args.STORE + '\\images\\df_meta.pkl'):
+        # Has the data already been partitioned?
+        if not os.path.isfile(args.STORE + '\\images\df_meta_train.pkl'):
+            df_meta = pd.read_pickle(args.STORE + '\\images\\df_meta.pkl')
+            # Check to see if the metadata has been updated with the file path of its samples:
+            if 'file_path' not in df_meta.columns:
+                # Update the df_meta dataframe with the file paths of it's samples:
+                df_meta = update_metadata_with_file_paths(df_meta)
+                # Pickle the dataframe so this code isn't run again:
+                df_meta.to_pickle(path=args.STORE + '\\images\\df_meta.pkl')
+            # Drop rows with a file_path of None (hopefully b/c http 404 and not string matching errors during reconstruct):
+            df_meta = df_meta.mask(df_meta.eq('None')).dropna(axis=0, how='all', subset=['file_path'])
+            ''' The exciting part! '''
+            # Drop everything but the file path and the target label from df_meta:
+            # df_meta = df_meta[['dwc:scientificName', 'file_path']]
+            # Partition into train and test splits:
+            df_train, df_test = model_selection.train_test_split(df_meta, test_size=0.2, shuffle=True)
+            ''' now actually move the files '''
+            if not os.path.isdir(args.STORE + '\\images\\train'):
+                os.mkdir(args.STORE + '\\images\\train')
+            if not os.path.isdir(args.STORE + '\\images\\test'):
+                os.mkdir(args.STORE + '\\images\\test')
+            # iterate through test data and move all files:
+            df_test_copy = df_test.copy(deep=True)
+            for i, row in df_test_copy.iterrows():
+                split_path = str.split(row['file_path'], sep='\\')
+                split_path.insert(-2, 'test')
+                desired_new_dir = '\\'.join(split_path[0:-1])
+                desired_file_path = '\\'.join(split_path)
+                if not os.path.isdir(desired_new_dir):
+                    os.mkdir(desired_new_dir)
+                try:
+                    # Attempt to move the file:
+                    shutil.move(row['file_path'], desired_file_path)
+                    # Update the metadata with the new file_path if successful:
+                    df_test.at[i, 'file_path'] = desired_file_path
+                except Exception as err:
+                    print(err)
+            df_test.to_pickle(path=args.STORE + '\\images\\df_test.pkl')
+            del df_test_copy
+            df_train_copy = df_train.copy(deep=True)
+            for i, row in df_train_copy.iterrows():
+                split_path = str.split(row['file_path'], sep='\\')
+                split_path.insert(-2, 'train')
+                desired_new_dir = '\\'.join(split_path[0:-1])
+                desired_file_path = '\\'.join(split_path)
+                if not os.path.isdir(desired_new_dir):
+                    os.mkdir(desired_new_dir)
+                try:
+                    # Attempt to move the file:
+                    shutil.move(row['file_path'], desired_file_path)
+                    # Update the metadata with the new file_path if successful:
+                    df_train.at[i, 'file_path'] = desired_file_path
+                except Exception as err:
+                    print(err)
+            df_train.to_pickle(path=args.STORE + '\\images\\df_train.pkl')
+            del df_train_copy
+    else:
+            print('df_meta.pkl not present, run the GoingDeeperImageDownloader to re-aggregate data.')
+            exit(-1)
+
+
 def main():
     # Declare globals:
     global args, use_gpu, metadata
@@ -217,31 +284,12 @@ def main():
         print('CUDA is enabled?: %s' % use_gpu)
     # Check if the data has already been partitioned into train, test, and validation datasets:
     if not os.path.isdir(args.STORE + '\\images\\train'):
-        # Data needs to be split into train, test, validate groups.
-        # Data should be split so that no collector ends up in both the training and testing sets.
-        # Load the metadata dataframe holding the collector of each sample:
-        if os.path.isfile(args.STORE + '\\images\\df_meta.pkl'):
-            # Has the data already been partitioned?
-            if not os.path.isfile(args.STORE + '\\images\df_meta_train.pkl'):
-                df_meta = pd.read_pickle(args.STORE + '\\images\\df_meta.pkl')
-                # Check to see if the metadata has been updated with the file path of its samples:
-                if 'file_path' not in df_meta.columns:
-                    # Update the df_meta dataframe with the file paths of it's samples:
-                    df_meta = update_metadata_with_file_paths(df_meta)
-                    # Pickle the dataframe so this code isn't run again:
-                    df_meta.to_pickle(path=args.STORE + '\\images\\df_meta.pkl')
-                # Drop rows with a file_path of None (hopefully b/c http 404 and not string matching errors during reconstruct):
-                df_meta = df_meta.mask(df_meta.eq('None')).dropna(axis=0, how='all', subset=['file_path'])
-                # Partition the data into training and testing datasets:
-                df_meta_train, df_meta_test, df_meta_val = partition_data(df_meta)
-                # df_meta_train.to_pickle(path=args.STORE + '\\images\\df_meta_train.pkl')
-                # df_meta_test.to_pickle(path=args.STORE + '\\images\\df_meta_test.pkl')
-
-        else:
-            print('df_meta.pkl not present, run the GoingDeeperImageDownloader to re-aggregate data.')
-            exit(-1)
+        partition_data()
+        pass
+        # df_meta_train.to_pickle(path=args.STORE + '\\images\\df_meta_train.pkl')
+        # df_meta_test.to_pickle(path=args.STORE + '\\images\\df_meta_test.pkl')
     # Get classifier training setup metadata:
-    metadata = get_metadata(df_meta)
+    # metadata = get_metadata(df_meta)
 
 
 if __name__ == '__main__':
