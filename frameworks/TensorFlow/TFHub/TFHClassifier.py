614c0d0b2000312af4cd82723bea6ab7b749ba87
ccampell <ccampell.asu@gmail.com>
2018-11-13 13:17:24 -0500

    Commited broken file with flag for where to resume after thesis meeting.

--- frameworks/TensorFlow/TFHub/TFHClassifier.py
+++ frameworks/TensorFlow/TFHub/TFHClassifier.py
@@ -16,6 +16,8 @@ from sklearn.exceptions import NotFittedError
 import tensorflow as tf
 import tensorflow_hub as hub
 from urllib.error import HTTPError
+# Custom decorator methods for ease of use when attaching TensorBoard summaries for visualization:
+from frameworks.TensorFlow.TFHub.CustomTensorBoardDecorators import attach_variable_summaries
 
 
 class TFHClassifier(BaseEstimator, ClassifierMixin):
@@ -42,14 +44,15 @@ class TFHClassifier(BaseEstimator, ClassifierMixin):
             computational graph for. For more information see:
             https://www.tensorflow.org/hub/api_docs/python/hub/ModuleSpec
         :param module_name: <str> The name to use for the key that will later allow this module to be retrieved via a
-            Tensorflow variable_scope. This name will be displayed for this module in TensorBoard.
+            TensorFlow variable_scope. This name will be displayed for this module in TensorBoard visualizations.
         :return tf_graph: The tf.Graph that was created.
-        :return bottleneck_tensor: <tf.placeholder> A bottleneck tensor representing the bottleneck values output by the
-            source module. This layer is the pre-activation layer just prior to the logits layer (the last fully
-            connected layer that feeds into the softmax layer). In other words, if the softmax layer is the final layer
-            in the neural network, then the logits layer is the dense fully-connected penultimate layer that
-            precedes it, and the bottleneck tensor is the layer that precedes this logits layer. For more information
-            on "bottlenecks" see: https://www.tensorflow.org/hub/tutorials/image_retraining#bottlenecks
+        :return bottleneck_tensor: <tf.Tensor> A bottleneck tensor representing the bottleneck values output by the
+            source module. This layer is the layer just prior to the logits layer; where the logits layer is defined as
+            the last fully-connected/pre-activation layer that feeds directly into the softmax layer. In other words,
+            if the softmax layer is the final layer in the neural network, then the logits layer is the dense
+            fully-connected penultimate layer that precedes the softmax layer. Proceeding this logits layer, is the
+            "Bottleneck Tensor". For more information on "bottlenecks" see:
+                https://www.tensorflow.org/hub/tutorials/image_retraining#bottlenecks
         """
         # Define the receptive field in accordance with the chosen architecture:
         height, width = hub.get_expected_image_size(tfhub_module_spec)
@@ -72,6 +75,28 @@ class TFHClassifier(BaseEstimator, ClassifierMixin):
                     tf.identity(bottleneck_tensor, name='bottleneck_tensor')
         return graph, bottleneck_tensor
 
+    @staticmethod
+    def _attach_variable_summaries(tf_variable):
+        """
+        _attach_variable_summaries: Attaches several TensorBoard summaries to the provided Tensor (for TensorBoard
+            visualization).
+        :param tf_variable: <tf.Variable (tf.Tensor wrapper)>  A TensorFlow tensor to which various TensorBoard
+            summaries are to be attached to. When a summary writer is created using this variable, these attached
+            summaries are evaluated and exported for visualization in TensorBoard.
+        :return None: Upon completion, the provided tf_variable will have had summary statistics attached to it that
+            will show in TensorBoard when exported via a summary writer.
+        """
+        with tf.name_scope('variable_summaries'):
+            mean = tf.reduce_mean(tf_variable)
+            tf.summary.scalar('mean', mean)
+            with tf.name_scope('stddev'):
+                stddev = tf.sqrt(tf.reduce_mean(tf.square(tf_variable - mean)))
+            tf.summary.scalar('stddev', stddev)
+            tf.summary.scalar('max', tf.reduce_max(tf_variable))
+            tf.summary.scalar('min', tf.reduce_min(tf_variable))
+            tf.summary.histogram('histogram', tf_variable)
+        return
+
     def _add_final_retrain_ops(self, num_unique_classes, bottleneck_tensor):
         """
         add_final_retrain_ops: Adds a new softmax and fully-connected layer for training and model evaluation. In order
@@ -86,11 +111,12 @@ class TFHClassifier(BaseEstimator, ClassifierMixin):
         :resource: https://github.com/tensorflow/hub/blob/master/examples/image_retraining/retrain.py
         :param num_unique_classes: <int> The number of unique classes in the training, validation, and testing datasets.
         :param bottleneck_tensor: <tf.Tensor> A bottleneck tensor representing the bottleneck values output by the
-            source module. This layer is the pre-activation layer just prior to the logits layer (the last fully
-            connected layer that feeds into the softmax layer). In other words, if the softmax layer is the final layer
-            in the neural network, then the logits layer is the dense fully-connected penultimate layer that
-            precedes it, and the bottleneck tensor is the layer that precedes this logits layer. For more information
-            on "bottlenecks" see: https://www.tensorflow.org/hub/tutorials/image_retraining#bottlenecks
+            source module. This layer is the layer just prior to the logits layer; where the logits layer is defined as
+            the last fully-connected/pre-activation layer that feeds directly into the softmax layer. In other words,
+            if the softmax layer is the final layer in the neural network, then the logits layer is the dense
+            fully-connected penultimate layer that precedes the softmax layer. Proceeding this logits layer, is the
+            "Bottleneck Tensor". For more information on "bottlenecks" see:
+                https://www.tensorflow.org/hub/tutorials/image_retraining#bottlenecks
         :
 
         :return:
@@ -134,22 +160,61 @@ class TFHClassifier(BaseEstimator, ClassifierMixin):
                     stddev=0.001
                 )
                 layer_weights = tf.Variable(initial_value=initial_value, name='final_weights')
-                # variable_summaries(layer_weights)
+                attach_variable_summaries(layer_weights)
 
             with tf.name_scope('biases'):
                 layer_biases = tf.Variable(initial_value=tf.zeros([num_unique_classes]), name='final_biases')
-                # variable_summaries(layer_biases)
+                attach_variable_summaries(layer_biases)
 
-            # pre-activations:
+            # pre-activations (z vector):
             with tf.name_scope('Wx_plus_b'):
+                '''
+                NOTE: The terminology gets a bit tricky right here, as the logits layer is the penultimate layer that
+                feeds directly into the softmax layer. The expression tf.matmul evaluates to z, that is: the weighted 
+                sum of the preceding layer's output (plus a bias term) BEFORE applying the activation function phi. 
+                The 'final_tensor' is the result of applying the activation function phi to the pre-activation layer z.
+                So where exactly are the logits? Normally we think of the logits layer as the result of applying the
+                activation function to z; that is, normally we think of the penultimate layer as the 'logits' layer. 
+                In this case however, the logits layer IS represented by the pre_activations variable, although it
+                appears as though it is only half of the step that makes up the logits layer. Upon applying the 
+                activation function (commonly softmax at this point) the resulting 'final_tensor' is the result of 
+                forward propagating the output from the logits layer directly into the final layer. In this case the
+                traditional 'softmax layer' is not an individual layer, but rather the result of directly applying
+                the activation function (softmax) to the pre-activation/logits layer (which directly precedes it). So,
+                there is a discrepancy from the way the textbook Hands on Machine Learning: ... visually represents the
+                workings of the network, and the actual implementation itself. However, the end result is the same:
+                a Tensor containing the predicted class label based upon the results of applying the chosen activation
+                function to the 
+                '''
+
+                '''
+                NOTE: The logits layer (or in the context of transfer learning, the: "bottleneck layer") is the 
+                penultimate layer that feeds directly into the 'layer that performs the actual classification' 
+                (in this case the softmax layer). During instantiation, the bottleneck_tensor is created to hold the
+                output of the bottleneck-layer/logits-layer computation. To be explicitly clear, the bottleneck tensor
+                is the same as logits tensor?
+                
+                
+                compute the  layer as a dense/fully-connected layer whose input is the so called
+                "bottleneck tensor" (e.g. the layer just before the final output layer that actually performs the 
+                classification). The logits layer 
+                
+                
+                I am confused.... seems like the bottleneck tensor is the logits layer and we are computing it twice...
+                https://www.tensorflow.org/hub/tutorials/image_retraining#bottlenecks
+                '''
+                ON RESUME ^ FINISH ABOVE COMMENT
                 logits = tf.matmul(bottleneck_input, layer_weights) + layer_biases
-                tf.summary.histogram('pre_activations_logits', logits)
+                tf.summary.histogram('pre_activations', pre_activations)
 
         raise NotImplementedError
 
     def __init__(self, tfhub_module_url):
         """
         __init__: Ensures the provided module url is valid, and stores it's hyperparameters for ease of reference.
+        NOTE: All estimators should specify all the parameters that can be set at the class level in their __init__ as
+            explicit keyword arguments (no *args or **kwargs).
+            See: https://scikit-learn.org/stable/modules/generated/sklearn.base.BaseEstimator.html
         :param tfhub_module_url: Which TensorFlow Hub module to instantiate, see the following url for some publicly
             available ones: https://github.com/tensorflow/hub/blob/r0.1/docs/modules/image.md
         """
