c12f58559807d06ab81b19c244a4d71331f6d164
ccampell <ccampell.asu@gmail.com>
2019-10-24 18:46:20 -0400

    Added training set code.

--- tests/ClassAccuracyTester.py
+++ tests/ClassAccuracyTester.py
@@ -182,22 +182,28 @@ def main(run_config):
 
     tfh_classifier = TrainedTFHClassifier(model_path=os.path.join(model_path, 'inference'), model_label_file_path=model_label_file_path)
     # tfh_classifier.load_model()
-    class_top_1_accuracies = tfh_classifier.calculate_class_top_1_accuracies(bottlenecks=val_bottlenecks, class_labels=class_labels)
-    class_top_5_accuracies = tfh_classifier.calculate_class_top_5_accuracies(bottlenecks=val_bottlenecks, class_labels=class_labels)
+    class_top_1_accuracies = tfh_classifier.calculate_class_top_1_accuracies(bottlenecks=train_bottlenecks, class_labels=class_labels)
+    class_top_5_accuracies = tfh_classifier.calculate_class_top_5_accuracies(bottlenecks=train_bottlenecks, class_labels=class_labels)
     top_1_accs = [value['top_1_acc'] for (key, value) in class_top_1_accuracies.items()]
     top_5_accs = [value['top_5_acc'] for (key, value) in class_top_5_accuracies.items()]
 
     print('Average top-1 Accuracy: %.2f%%' % (sum(top_1_accs)/len(top_1_accs)))
     print('Average top-5 Accuracy: %.2f%%' % (sum(top_5_accs)/len(top_5_accs)))
+
+    with open('top_1_accuracies_by_class_train_set.json', 'w') as fp:
+        json.dump(class_top_1_accuracies, fp, indent=4, separators=(',', ': '))
+    with open('top_5_accuracies_by_class_train_set.json', 'w') as fp:
+        json.dump(class_top_5_accuracies, fp, indent=4, separators=(',', ': '))
+
     # with open('top_1_accuracies_by_class_test_set.json', 'w') as fp:
     #     json.dump(class_top_1_accuracies, fp, indent=4, separators=(',', ': '))
     # with open('top_5_accuracies_by_class_test_set.json', 'w') as fp:
     #     json.dump(class_top_5_accuracies, fp, indent=4, separators=(',', ': '))
 
-    with open('top_1_accuracies_by_class_val_set.json', 'w') as fp:
-        json.dump(class_top_1_accuracies, fp, indent=4, separators=(',', ': '))
-    with open('top_5_accuracies_by_class_val_set.json', 'w') as fp:
-        json.dump(class_top_5_accuracies, fp, indent=4, separators=(',', ': '))
+    # with open('top_1_accuracies_by_class_val_set.json', 'w') as fp:
+    #     json.dump(class_top_1_accuracies, fp, indent=4, separators=(',', ': '))
+    # with open('top_5_accuracies_by_class_val_set.json', 'w') as fp:
+    #     json.dump(class_top_5_accuracies, fp, indent=4, separators=(',', ': '))
 
 
 if __name__ == '__main__':
